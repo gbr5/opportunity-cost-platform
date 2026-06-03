a29fbcdf3d16f36c80b63f7efb1d1960a95d3a98
Guy Birchal Robbe - Mac <gbrobbe@gmail.com>
2026-06-02 21:40:50 -0300

    Add ts-ignore to Prisma import

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,5 +1,6 @@
-import { PrismaPg } from '@prisma/adapter-pg';
+// @ts-ignore
 import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
 import { Pool } from 'pg';
 
 const pool = new Pool({
